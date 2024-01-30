4eb49bd9d291ed418e780b04a33f705b399a817e
Beserker-356 <141203631+Beserker-356@users.noreply.github.com>
2024-01-30 19:23:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+var c1 = document.querySelector("#chime1T");
+var c2 = document.querySelector("#chime2T");
+var c3 = document.querySelector("#chime3T");
+var button = document.querySelector("#button");
+const audio = new Audio("wind.mp3");
+audio.loop = true
+
+c1.addEventListener("mouseout", function () {
+  audio.play();
+  document.querySelector("#chime1").style.animationName = "swing";
+  setTimeout(() => {
+    document.querySelector("#chime1").style.animationName = "none";
+    audio.pause();
+  }, 1750);
+});
+c2.addEventListener("mouseout", function () {
+  audio.play();
+  document.querySelector("#chime2").style.animationName = "swing";
+  setTimeout(() => {
+    document.querySelector("#chime2").style.animationName = "none";
+    audio.pause();
+  }, 1500);
+});
+c3.addEventListener("mouseout", function () {
+  audio.play();
+  document.querySelector("#chime3").style.animationName = "swing";
+  setTimeout(() => {
+    document.querySelector("#chime3").style.animationName = "none";
+    audio.pause()
+  }, 1250);
+});
+
+const pendulum = document.querySelector("#pendulum");
+button.addEventListener("click", function () {
+  if (button.innerText === "START") {
+    button.innerText = "STOP"
+    pendulum.style.animationName =
+      "pendulum";
+    let sec;
+    let min;
+    let hr;
+    let timerID = setInterval(() => {
+      let d = new Date();
+      sec = d.getSeconds() * 6;
+      min = d.getMinutes() * 6;
+      hr = d.getHours() * 30 + Math.round(min / 12);
+      if (button.innerText === "STOP") {
+        pendulum.style.animationName = "pendulum";
+        setInterval(() => {}, 800);
+      }
+      document.querySelector("#sec").style.transform = "rotate(" + sec + "deg)";
+      document.querySelector("#min").style.transform = "rotate(" + min + "deg)";
+      document.querySelector("#hr").style.transform = "rotate(" + hr + "deg)";
+    }, 1000);
+  }
+  else {
+    location.reload()
+  }
+});
+
+

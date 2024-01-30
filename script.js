3488cef9ec3a223b8b1731dca54a6dd4fcff510f
Beserker-356 <141203631+Beserker-356@users.noreply.github.com>
2024-01-30 20:12:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ var c2 = document.querySelector("#chime2T");
 var c3 = document.querySelector("#chime3T");
 var button = document.querySelector("#button");
 const audio = new Audio("wind.mp3");
-audio.loop = true
+audio.loop = true;
+let isRunning = false;
 
 c1.addEventListener("mouseout", function () {
   audio.play();
@@ -26,36 +27,38 @@ c3.addEventListener("mouseout", function () {
   document.querySelector("#chime3").style.animationName = "swing";
   setTimeout(() => {
     document.querySelector("#chime3").style.animationName = "none";
-    audio.pause()
+    audio.pause();
   }, 1250);
 });
 
 const pendulum = document.querySelector("#pendulum");
 button.addEventListener("click", function () {
   if (button.innerText === "START") {
-    button.innerText = "STOP"
-    pendulum.style.animationName =
-      "pendulum";
-    let sec;
-    let min;
-    let hr;
-    let timerID = setInterval(() => {
-      let d = new Date();
-      sec = d.getSeconds() * 6;
-      min = d.getMinutes() * 6;
-      hr = d.getHours() * 30 + Math.round(min / 12);
-      if (button.innerText === "STOP") {
-        pendulum.style.animationName = "pendulum";
-        setInterval(() => {}, 800);
-      }
-      document.querySelector("#sec").style.transform = "rotate(" + sec + "deg)";
-      document.querySelector("#min").style.transform = "rotate(" + min + "deg)";
-      document.querySelector("#hr").style.transform = "rotate(" + hr + "deg)";
-    }, 1000);
-  }
-  else {
-    location.reload()
+    button.innerText = "STOP";
+    isRunning = true;
+  } else {
+    button.innerText = "START";
+    isRunning = false;
+    pendulum.style.animationName = "none";
   }
 });
 
-
+let sec;
+let min;
+let hr;
+setInterval(() => {
+  if (isRunning) {
+    pendulum.style.animationName = "pendulum";
+    let d = new Date();
+    sec = d.getSeconds() * 6;
+    min = d.getMinutes() * 6;
+    hr = d.getHours() * 30 + Math.round(min / 12);
+    if (button.innerText === "STOP") {
+      pendulum.style.animationName = "pendulum";
+      setInterval(() => {}, 800);
+    }
+    document.querySelector("#sec").style.transform = "rotate(" + sec + "deg)";
+    document.querySelector("#min").style.transform = "rotate(" + min + "deg)";
+    document.querySelector("#hr").style.transform = "rotate(" + hr + "deg)";
+  }
+}, 1000);
